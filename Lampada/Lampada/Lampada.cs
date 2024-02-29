97fcbe6f918b1ce8ccc8c94dbb0987781418cb28
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-02-29 12:35:15 -0300

    Add files via upload

---
+++ Lampada/Lampada/Lampada.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lampada
+{
+    public class Lampada
+    {
+        private bool Ligada { get; set; }
+        private int Potencia { get; set; }
+        private string? Cor { get; set; }
+
+        public void Ligar()
+        {
+            Ligada = true;
+        }
+        public void Desigar()
+        {
+            Ligada = false;
+        }
+        public bool TarLigadar()
+        {
+            return Ligada;
+        }
+        public void QueCor(string fussia)
+        {
+            Cor = fussia;
+        }
+        public void TaPotente(int potente)
+        {
+            Potencia = potente;
+        }
+        public string corzinha()
+        {
+            return Cor;
+        }
+        public int poder()
+        { 
+            return Potencia; 
+        }
+    }
+}
---
+++ Lampada/Lampada/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //-------------Objeto--------------
+            Lampada lampadanovamlk = new Lampada();
+
+            //--------------Chamando método pra ligar a lampada--------
+
+            //--------------Método------------------
+            lampadanovamlk.Ligar();
+
+            lampadanovamlk.QueCor("Rosa");
+
+            lampadanovamlk.TaPotente(120);
+
+            Console.WriteLine("A lampada está ligada? " + lampadanovamlk.TarLigadar());
+            Console.WriteLine("A cor da lampada é " + lampadanovamlk.corzinha());
+            Console.WriteLine("a Potencia é de " + lampadanovamlk.poder());
+        }
+    }
+}
