ff878059dbc375e6180d04c4ff6549befe0039a6
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-03-14 12:32:53 -0300

    Add files via upload

---
+++ Conta/Conta/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+    public class Conta
+    {
+        public string? num_conta { get; set; }                          
+        public float saldo { get; set; }        
+        private float limite { get; set; }
+
+        public float consulta_saldo()
+        {
+            return saldo;
+        }
+        public void deposito(float dep)
+        {
+            saldo += dep;
+            Console.WriteLine("Seu saldo após o deposito é de:R$ " + saldo);
+            Console.WriteLine("Você pode sacar até R$" + (saldo + limite));
+        }
+
+        public void ajusta_limite(float lim)
+        {
+            limite = lim;
+            Console.WriteLine("Seu limite é de R$ " + limite);
+        }
+        public void sacar(float saque)
+        {
+            if (saque <= saldo + limite)
+            {
+                saldo -= saque;
+            }
+            else
+            {
+                Console.WriteLine("Você não possui saldo o suficiente para sacar");
+            }
+        }
+    }
+}
---
+++ Conta/Conta/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Conta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();  
+            
+            conta.ajusta_limite(700);
+
+            conta.deposito(900);
+
+            conta.consulta_saldo());
+
+            conta.sacar(300);
+
+            conta.consulta_saldo());
+        }
+    }
+}
