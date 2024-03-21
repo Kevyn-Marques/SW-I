4557521dde5b8adec8e791c235415c9310c90d99
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-03-21 08:22:14 -0300

    Add files via upload

---
+++ Aviao/Aviao/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aviao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aviao aviao = new Aviao();
+
+            aviao.Marca = "Airbus";
+            aviao.Modelo = "a380";
+
+            aviao.Acelerar(8);
+            aviao.Subir(420);
+
+            Console.WriteLine($"A altura do avião é: {aviao.Altitude}m, A velocidade é: {aviao.Velocidade}km/h, A marca é: {aviao.Marca}, e o modelo é: {aviao.Modelo}");
+        }
+    }
+}
