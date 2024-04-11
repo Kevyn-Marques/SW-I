5da8d2bdb78174768367f2a83e599fde0928dfe0
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-04-11 12:39:22 -0300

    Add files via upload

---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Area.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaQuadradoRetangulo
+{
+    public class Area
+    {
+        public float Base { get; set; }
+
+        public float Altura { get; set; }   
+
+        public float AreaTotal { get; set; }
+
+        public string Calcular()
+        {
+            Console.WriteLine("Digite a base:");
+            Base = float.Parse(Console.ReadLine() );
+            Console.WriteLine("Digite a altura:");
+            Altura= float.Parse(Console.ReadLine() );
+
+            AreaTotal = Base * Altura;
+
+            string tipo = " ";
+
+            if(Base == Altura)
+            {
+                 tipo = " e é um Quadrado";
+            }
+            else
+            {
+                tipo = " e é um Retângulo";
+            }
+
+            string resultado = "A Àrea é " + AreaTotal;
+            resultado += tipo; 
+
+            return resultado;
+
+        }
+        public string Dados()
+        {
+            string informacao = "O valor da Base é:" + Base ;
+            informacao += "\nO Valor Altura é:" + Altura ;
+            return informacao;
+        }
+    }
+}
---
+++ Exercicios/AreaQuadradoRetangulo/AreaQuadradoRetangulo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AreaQuadradoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Àrea Quadrado/Retângulo");
+
+            Area pimparino = new Area();
+            Console.WriteLine(pimparino.Calcular());
+            Console.WriteLine(pimparino.Dados());
+        }
+    }
+}
