136d5a0aeff89f126f4618401e4f8d03a270f1cf
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-04-11 12:38:29 -0300

    Add files via upload

---
+++ Exercicios/Pessoa/Pessoa/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoa
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public DateTime DatadeNascimento { get; set; }
+
+        public float Altura { get; set; }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Data de Nascimento: " + DatadeNascimento);
+            Console.WriteLine("Altura: " + Altura);
+        }
+        public int CalcularIdade()
+        {
+            return DateTime.Today.Year - DatadeNascimento.Year;
+        }
+    }
+}
---
+++ Exercicios/Pessoa/Pessoa/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Pessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Joel";
+            p.DatadeNascimento = new DateTime(1993, 6, 28);
+            p.Altura = 1.76f;
+
+            p.ImprimirDados();
+
+            int idade = p.CalcularIdade();
+
+            Console.WriteLine("Idade: " + idade);
+        }
+    }
+}
