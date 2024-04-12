720c3dc2d40e0db14eaadfdaef1a0b05ace72e95
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-04-12 12:09:06 -0300

    Update Program.cs

--- Exercicios/Salario/Salario/Program.cs
+++ Exercicios/Salario/Salario/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Salario sal = new Pagamento();
+            sal.Nome = "Carlos Da Shell";
+            sal.Sal = 1345;
+            sal.HED = 0;
+            sal.HEN = 64;
+            sal.ND = 0;
+            sal.Fal = 52;
+            sal.DE = 5;
+            sal.REF = 5;
+            sal.Val = 4;
+            Console.WriteLine(p.FolhaDePagamento());
         }
     }
 }
