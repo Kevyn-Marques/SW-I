041b7a685e70ac5d045103c6e7b7855d5e1024ca
Kevyn Marques <124791702+Kevyn-Marques@users.noreply.github.com>
2024-04-12 12:06:09 -0300

    Update Salario.cs

--- Exercicios/Salario/Salario/Salario.cs
+++ Exercicios/Salario/Salario/Salario.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Salario
 {
-    internal class Salario
-    {
+    public class Salario
+   {
+        public string Nome { get; set; }
+        public float Sal { get; set; }
+        public float HED { get; set; }
+        public float HEN { get; set; }
+        public int ND { get; set; }
+        public float Fal { get; set; }
+        public float DE { get; set; }
+        public float REF { get; set; }
+        public float Val { get; set; }
+        
+        public string FolhaDePagamento()
+        {
+            double HoraExtra = HED * Sal / 160 + HEN * 1.2 * Sal / 160;
+            double SalF = ND * 0.05 * 1412;
+            double INAMPS = 0.08 * Sal;
+            double faltas = Fal * Sal / 160;
+            double SalB = Sal + HoraExtra + SalF;
+            double imposto_de_renda = 0.08 * Sal;
+
+            return $"Nome: {Nome}\nSalário: {Sal}\nHoras Extras: {HoraExtra}\nSalário Família: {SalF}\nSalário Bruto: {SalB}\nINAMPS: {INAMPS}\nFaltas: {faltas}\nRefeições: {REF}\nVales: {Val}\nDescontos Eventuais: {DE}\nImposto de renda: {imposto_de_renda}\nSalário Líquido: {SalB - INAMPS - faltas - REF - Val - DE - imposto_de_renda}";
+        }
     }
 }
